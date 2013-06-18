7deab833a5889f8af44cb8c5969f71e29a29e6bc
Jozsef Kozma <joco@bern.local>
2013-06-18 21:31:35 +0200

    the right way to copy a whole directory

--- modules/people/manifests/jzsfkzm.pp
+++ modules/people/manifests/jzsfkzm.pp
@@ -4,19 +4,16 @@ class people::jzsfkzm {
   include daisy_disk
   include vlc
 
-  file { "$home/bin":
-    ensure => 'directory',
-    owner => $luser,
-    group => 'staff',
-    mode => 775
-  }
-
   file { "$home/.gitconfig":
     source => "puppet:///modules/people/manifests/jzsfkzm/files/.gitconfig"
   }
 
   file { "$home/bin":
-    source => "puppet:///modules/people/manifests/jzsfkzm/files/bin",
+    ensure => 'directory',
+    owner => $luser,
+    group => 'staff',
+    mode => 775,
+    source => "$boxen::config::repodir/modules/people/manifests/jzsfkzm/files/bin",
     recurse => true
   }
 
