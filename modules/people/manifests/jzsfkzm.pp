5852b77bb706b255b50ab6bb130ec2417caf9464
Jozsef Kozma <joco@gawker.com>
2013-06-20 07:51:01 +0200

    these things moved to my personal dotfiles repo

--- modules/people/manifests/jzsfkzm.pp
+++ modules/people/manifests/jzsfkzm.pp
@@ -4,26 +4,7 @@ class people::jzsfkzm {
   include daisy_disk
   include vlc
 
-  file { "$home/.gitconfig":
-    source => "$boxen::config::repodir/modules/people/manifests/jzsfkzm/files/.gitconfig"
+  repository { "$home/.dotfiles":
+    source => 'jzsfkzm/dotfiles'
   }
-
-  file { "$home/.bash_profile":
-    source => "$boxen::config::repodir/modules/people/manifests/jzsfkzm/files/.bash_profile"
-  }
-
-  file { "$home/Library/Application Support/Sublime Text 2/Packages/User/Preferences.sublime-settings":
-    source => "$boxen::config::repodir/modules/people/manifests/jzsfkzm/files/Preferences.sublime-settings"
-  }
-
-  file { "$home/bin":
-    ensure => 'directory',
-    owner => $luser,
-    group => 'staff',
-    mode => 775,
-    source => "$boxen::config::repodir/modules/people/manifests/jzsfkzm/files/bin",
-    recurse => true
-  }
-
-  include people::jzsfkzm::preferences
 }
