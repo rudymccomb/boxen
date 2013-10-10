d753d98ea52e85b9d5768c31c7d0d41f1133b8dd
Jozsef Kozma <joco@gawker.com>
2013-10-10 13:38:09 +0200

    my own stuff

--- modules/people/manifests/jzsfkzm.pp
+++ modules/people/manifests/jzsfkzm.pp
@@ -1,7 +1,11 @@
 class people::jzsfkzm {
   $home = "/Users/${::luser}"
 
+  include calibre
+  include cloudapp
   include daisy_disk
+  include googledrive
+  include steam
   include vlc
 
   repository { "$home/.dotfiles":
